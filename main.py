c2ed1211d7e5814adec8fc89ff6af0521f8f5da4
Shomil Jain <shomil@Shomils-MacBook-Pro.local>
2021-01-30 16:54:04 -0800

    gcp

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import escape, jsonify
+from utils import query
+
+def hello_http(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    """
+    request_json = request.get_json(silent=True)
+
+    if request_json and 'columns' in request_json and 'filters' in request_json:
+        columns, filters = request_json['columns'], request_json['filters']
+        return jsonify(query(columns, filters).to_csv())
+    else:
+        return jsonify({'error': 'unknown'})
